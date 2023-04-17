2931bc942c035cc9a95bf7c6d31b20c1200c6f84
dileep765 <penugudurudileep@gmail.com>
2023-04-17 10:54:32 +0000

    Super Over League

---
+++ index.js
@@ -0,0 +1,11 @@
+const element = (
+//   Write your code here.
+   <div className = "super-over-league-container">
+       <h1 class="heading">Super Over League</h1>
+   </div>
+   <img  src="https://assets.ccbp.in/frontend/react-js/rcb-img.png" className = "image"/>
+   <img src="https://assets.ccbp.in/frontend/react-js/csk-img.png"  className = "image"/>
+
+)
+
+ReactDOM.render(element, document.getElementById('root'))
